4b874a3f39b4c05510fb50a2434ce437dec1571d
Gita Alekhya Paul <54375111+gitaalekhyapaul@users.noreply.github.com>
2021-07-06 20:26:38 +0530

    🐛 limit API calls

--- components/Inputs/PincodeSelect.tsx
+++ components/Inputs/PincodeSelect.tsx
@@ -13,16 +13,18 @@ const PincodeSelect = ({ getPincode, ...props }: { [x: string]: any }) => {
         const newOptions: Array<{ name: string; value: string }> = [
           { name: "Select Option", value: "" },
         ];
-        const { data } = await axios.get(
-          `https://api.data.gov.in/resource/6176ee09-3d56-4a3b-8115-21841576b2f6?api-key=${process.env.NEXT_PUBLIC_API_KEY}&format=json&skip=0&limit=100&filters[pincode]=${getPincode}`
-        );
-        if (data.records) {
-          data.records.map((record: { officename: string }) => {
-            newOptions.push({
-              name: record.officename,
-              value: record.officename,
+        if (/^7[0-9]{5}$/.test(getPincode)) {
+          const { data } = await axios.get(
+            `https://api.data.gov.in/resource/6176ee09-3d56-4a3b-8115-21841576b2f6?api-key=${process.env.NEXT_PUBLIC_API_KEY}&format=json&skip=0&limit=100&filters[pincode]=${getPincode}`
+          );
+          if (data.records) {
+            data.records.map((record: { officename: string }) => {
+              newOptions.push({
+                name: record.officename,
+                value: record.officename,
+              });
             });
-          });
+          }
         }
         setOptions(newOptions);
       } catch (err) {
